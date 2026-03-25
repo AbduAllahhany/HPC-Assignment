f7016619744eaba4b2e48f2c89d7b073560de3f5
Mahmoud Khattab <mahmoud403178@feng.bu.edu.eg>
2026-03-25 07:15:41 +0200

    add Bitonic Sort

---
+++ src/include/serial_bitonic_sort.h
@@ -0,0 +1,9 @@
+#ifndef ASSINMENT_SERIAL_BITONIC_SORT_H
+#define ASSINMENT_SERIAL_BITONIC_SORT_H
+
+#include <cstdint>
+#include <vector>
+
+void serialBitonicSort(std::vector<int32_t> &arr);
+
+#endif //ASSINMENT_SERIAL_BITONIC_SORT_H
